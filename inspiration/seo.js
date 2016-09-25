4061596d19dae8ae4553c337ae97d9dfd205cabc
ansteh <stehle.andre@googlemail.com>
2016-09-25 11:22:24 +0200

    seo links

---
+++ inspiration/seo.js
@@ -0,0 +1,6 @@
+https://github.com/marcobiedermann/search-engine-optimization
+https://moz.com/blog
+https://moz.com/beginners-guide-to-seo
+https://moz.com/learn/seo
+https://developer.mozilla.org/de/docs/Web/Barrierefreiheit/ARIA
+https://schema.org/

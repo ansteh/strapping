1f32440278e5e3105d7e0f31ba7e4e6681a1251f
ansteh <stehle.andre@googlemail.com>
2016-10-06 00:15:39 +0200

    seo

--- inspiration/seo.js
+++ inspiration/seo.js
@@ -4,3 +4,18 @@ https://moz.com/beginners-guide-to-seo
 https://moz.com/learn/seo
 https://developer.mozilla.org/de/docs/Web/Barrierefreiheit/ARIA
 https://schema.org/
+
+create sitemap:
+  https://support.google.com/webmasters/answer/183668?hl=en
+
+canonical URLs (definition):
+  https://support.google.com/webmasters/answer/139066
+
+build and submit sitemap:
+  https://support.google.com/webmasters/answer/183668?hl=en#sitemapformat
+
+node module sitemap:
+  https://www.npmjs.com/package/sitemap#example-of-indicating-alternate-language-pages
+
+submit either to google by console or add it to robots.txt:
+  https://support.google.com/webmasters/answer/183668?hl=en#addsitemap

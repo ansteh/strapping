0566bed8a399a2331b5706816d48e89d8953d70b
ansteh <stehle.andre@googlemail.com>
2017-01-19 00:59:27 +0100

    pareto dataset generation

--- python/pareto.py
+++ python/pareto.py
@@ -3,25 +3,17 @@ from scipy.stats import pareto
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots(1, 1)
 
-b = 2.62
+a, m = 20., 2.  # shape and mode
+s = (np.random.pareto(a, 100) + 1) * m
 
-x = np.linspace(pareto.ppf(0.01, b), pareto.ppf(0.99, b), 100)
-ax.plot(x, pareto.pdf(x, b), 'r-', lw=5, alpha=0.6, label='pareto pdf')
+count, bins = np.histogram(s, 100, normed=True)
+fit = a*m**a / bins**(a+1)
+# print bins
 
-r = pareto.rvs(b, size=1000)
-print r
-ax.hist(r, normed=True, histtype='stepfilled', alpha=0.2)
-ax.legend(loc='best', frameon=False)
+samples = fit / np.max(fit)
+samples = np.round(samples*50)
+print samples
+
+plt.plot(bins, samples)
 
 plt.show()
-
-# a, m = 50., 100.  # shape and mode
-# s = (np.random.pareto(a, 1000) + 1) * m
-#
-# import matplotlib.pyplot as plt
-# count, bins, _ = plt.hist(s, 100, normed=True)
-# print bins
-# fit = a*m**a / bins**(a+1)
-# print fit
-# plt.plot(bins, max(count)*fit/max(fit), linewidth=2, color='r')
-# plt.show()
